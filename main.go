8eb22211427821e75c76a98b49caed4fc1cfad0c
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2024-08-23 05:17:04 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "time"
+
+    "github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+    Use:   "mycli",
+    Short: "My CLI tool",
+    Long:  `A longer description of your CLI tool.`,
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Println("Hello from my CLI tool!")
+    },
+}
+
+var dayOfWeekCmd = &cobra.Command{
+    Use:   "dayofweek",
+    Short: "Prints the current day of the week",
+    Long:  `This command prints the current day of the week.`,
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Println("Today is", time.Now().Weekday())
+    },
+}
+
+func main() {
+    rootCmd.AddCommand(dayOfWeekCmd)
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+}
